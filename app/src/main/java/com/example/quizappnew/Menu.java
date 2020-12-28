915a165991a8836005fd9f0f335acff26baf48e0
Puelro <74191137+Puelro@users.noreply.github.com>
2020-12-28 15:18:55 +0100

    Menu Highscore Play sind Clickable

--- app/src/main/java/com/example/quizappnew/Menu.java
+++ app/src/main/java/com/example/quizappnew/Menu.java
@@ -2,13 +2,37 @@ package com.example.quizappnew;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
+
+    Button buttonPlay;
+    Button buttonHighscore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        buttonPlay = findViewById(R.id.btnPlay);
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this,Play.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonHighscore = findViewById(R.id.btnHighscore);
+        buttonHighscore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this,Highscore.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/quizappnew/Play.java
+++ app/src/main/java/com/example/quizappnew/Play.java
@@ -2,13 +2,28 @@ package com.example.quizappnew;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Play extends AppCompatActivity {
+
+    Button buttonMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+
+        buttonMenu = findViewById(R.id.btnMenu);
+        buttonMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Play.this,Menu.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
