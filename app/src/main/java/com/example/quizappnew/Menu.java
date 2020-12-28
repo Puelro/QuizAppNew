54257698586fb7a4628e4ebe71e4c0f8f1c14cf1
Puelro <74191137+Puelro@users.noreply.github.com>
2020-12-28 14:31:36 +0100

    fehler behoben

--- app/src/main/java/com/example/quizappnew/Menu.java
+++ app/src/main/java/com/example/quizappnew/Menu.java
@@ -9,6 +9,6 @@ public class Menu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.Menu);
+        setContentView(R.layout.activity_menu);
     }
 }
